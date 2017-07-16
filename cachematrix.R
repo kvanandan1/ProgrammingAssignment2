bf3345663734f08b1f3df0556fc05380544b69cb
kvanandan1 <kvanandan1@yahoo.com>
2017-07-16 11:23:55 +0530

    saved

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## Programming Assignment 2
+## Programming Assignment
 ## Caching the Inverse of a Matrix:
 ## Matrix inversion is usually a costly computation and there may be some 
 ## benefit to caching the inverse of a matrix rather than compute it repeatedly.
